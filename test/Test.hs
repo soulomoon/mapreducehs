713b25e26a62596b0c2247990af5c79239264742
Patrick <fwy996602672@gmail.com>
2023-02-19 15:07:14 +0800

    add test

---
+++ test/Test.hs
@@ -0,0 +1,25 @@
+module Main where
+
+import Test.HUnit
+import Core.MapReduceC
+
+foo :: Int -> (Int, Int)
+foo 3 = (1, 2)
+foo _ = (0, 0)
+
+partA :: Int -> IO (Int, Int)
+partA _ = pure (5, 3) -- change to (5,3) if you want the tests to succeed
+
+partB :: Int -> IO Bool
+partB x = pure (x == 3) 
+
+test1, test2 :: Test
+test1 = TestCase $ assertEqual "for (foo 3)," 1 1
+test2 = TestCase $ do
+  (x, y) <- partA 3
+  assertEqual "for the first result of partA," 5 x
+  b <- partB y
+  assertBool ("(partB " ++ show y ++ ") failed") b
+
+main :: IO ()
+main = runTestTTAndExit (TestList [TestLabel "test1" test1])
